592e9a4c29d6b65d9c7c3fdeebf56647a76ea511
Kanokporn1132 <Kanokporn1132@users.noreply.github.com>
2020-02-02 22:45:51 +0700

    Update Exercise1.ino

--- lab2/Exercise1.ino
+++ lab2/Exercise1.ino
@@ -3,36 +3,33 @@ int data_pin = 5; //DS
 int latch_pin = 4; //STCP
 
 
-void brokenmicro1(int DATA) {
+void brokenmicro(int DATA) {
   digitalWrite(latch_pin, LOW);
   for(int i = 0 ; i < 16 ; i++){
+    delay(10);
+    // compare byte level
     digitalWrite(data_pin, (DATA >> (15-i)) & 0b0000000000000001);
-    //digitalWrite(data_pin, 0b0000000000000000);
     digitalWrite(clk_pin,HIGH);
     digitalWrite(clk_pin,LOW);
-    digitalWrite(latch_pin,HIGH);
-    delay(100);
-    digitalWrite(latch_pin,LOW);
   }
-//  digitalWrite(latch_pin,HIGH);
-//  delay(100);
-//  digitalWrite(latch_pin,LOW);
+  digitalWrite(latch_pin,HIGH);
+  delay(10);
 }
+
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(clk_pin,OUTPUT);
   pinMode(latch_pin,OUTPUT);
-    delay(100);
   pinMode(data_pin,OUTPUT);
+  
 }
-void loop() {
-  // put your main code here, to run repeatedly:
-  int data = 0b1000000000000000;
-  
-  for(int i = 0; i < 16; i++){
-    brokenmicro1(data);
-  }
 
-  
+
+void loop() {
+  int data = 0b1000000000000000;
+
+  for(int i = 0; i < 15; i++){
+    brokenmicro(data>>i);
+    delay(100);
+  }
 }
