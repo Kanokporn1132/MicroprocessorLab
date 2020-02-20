9c0227081677fffa6652c5ecdbbc2debddd82f7a
Kanokporn1132 <Kanokporn1132@users.noreply.github.com>
2020-02-20 15:01:50 +0700

    Update exercise_1.ino

--- lab5/exercise_1.ino
+++ lab5/exercise_1.ino
@@ -1,21 +1,35 @@
-#include <ThreeWire.h>
-#include <RtcDS1302.h>
+#include <SPI.h>
+#define DATA_PIN 11
+#define CLOCK_PIN 13
+#define SS_PIN_1 10
+#define SS_PIN_2 9
+int ledshow[10] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
 
-ThreeWire myWire(4,5,2); // IO, SCLK, CE
-RtcDS1302<ThreeWire> Rtc(myWire);
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(57600);
-  
+void spi_write_1 (unsigned char cData){
+  shiftOut(DATA_PIN,CLOCK_PIN, MSBFIRST, cData);
+  digitalWrite (SS_PIN_1,HIGH);
+  digitalWrite (SS_PIN_1,LOW);
+}
+void spi_write_2 (unsigned char cData){
+  shiftOut(DATA_PIN,CLOCK_PIN, MSBFIRST, cData);
+  digitalWrite (SS_PIN_2,HIGH);
+  digitalWrite (SS_PIN_2,LOW);
+}
+void show_num(int x)
+{
+  spi_write_1(ledshow[x/10]);
+  spi_write_2(ledshow[x%10]);
 }
 
-void loop() {
-  RtcDateTime now = Rtc.GetDateTime();
-  Serial.println(now.Second());
-  if (now.Second()%2 == 0){
-    digitalWrite(10, HIGH);
-    delay(1000);
-    digitalWrite(10, LOW); 
+void setup(){
+  pinMode(SS_PIN_1,OUTPUT);
+  pinMode(DATA_PIN,OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+}
+void loop(){
+    int a;
+  for(a=0;a<100;a++){
+    show_num(a);
+  delay(500);
   }
 }
